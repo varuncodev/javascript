16795bf362d8fe7c9df346ff2574824d6c1599ee
varuncodev <varuncodev@gmail.com>
2026-03-26 15:44:21 +0530

    array part 1

--- variable.js
+++ variable.js
@@ -7,4 +7,31 @@
 // console.log(undefined >=0);
 
  
+// Array
+//  An array in JavaScript is a special variable used to store multiple values in a single container.
+// Nested objects	Shared reference	Fully copied
+// Performance	Faster	Slower
+// Memory usage	Less	More
+// Safe for mutation	❌ No	✅ Yes
+// Shallow copy = Copy of a house, but same furniture inside
+// Deep copy = Copy of house + completely new furniture
 
+const arr =[1,2,4,6,7,8]
+const newarr =["sfse","sefs","Sfsf"]
+console.log(arr); 
+console.log(typeof newarr[2])
+
+
+
+const myarr2 =new Array(1,2,3,4,5)
+console.log(myarr2)
+
+myarr2.push(6)
+console.log(myarr2)
+
+myarr2.pop()
+console.log(myarr2)         
+myarr2.unshift(9)       
+console.log(myarr2) 
+myarr2.shift()
+console.log(myarr2)
