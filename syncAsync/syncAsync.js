845cf064cc73fe4c7929d48dee3f963e57358b06
varuncodev <varuncodev@gmail.com>
2026-04-02 00:43:25 +0530

    syncAsync

---
+++ syncAsync/syncAsync.js
@@ -0,0 +1,36 @@
+// console.log("task1");
+// setTimeout(()=>{
+//     console.log("task2");
+
+// },2000)
+
+
+// /*
+// setTimeout
+// setinterval
+// axios
+// fetch
+// */
+
+
+// console.log("task3  ");
+
+
+
+
+console.log("task1");
+console.log("task2");
+
+setTimeout(()=>{
+    console.log("task3");
+},1000)
+
+console.log("task4");
+
+setTimeout(()=>{
+    console.log("task4");
+},1000)
+
+console.log("task5");
+
+console.log("task6");
